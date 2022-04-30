bb079e961263e89e174eba6fbdeb475bea6720e6
libramusician <dongguanqun@126.com>
2022-04-30 17:37:37 -0400

    add view

--- App/src/main/java/com/Calculator.java
+++ App/src/main/java/com/Calculator.java
@@ -1,4 +1,12 @@
 package com;
 
 public class Calculator {
+
+    public Calculator() {
+        CalculatorView view = new CalculatorView();
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+    }
 }
---
+++ GUI/src/main/java/com/CalculatorView.java
@@ -0,0 +1,13 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorView extends JFrame {
+    public CalculatorView(){
+        super("Calculator");
+        setSize(500,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
